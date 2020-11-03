d273ed9be5cc80348e53ad26c81a5062112c09a6
Altino Júnior <altinoalvesjunior@gmail.com>
2020-11-03 10:18:49 -0300

    ATL: Fim da Classe Nó, Inserção de Métodos na classe Fila

--- Códigos/AulaoTAD-Fila/src/Fila.java
+++ Códigos/AulaoTAD-Fila/src/Fila.java
@@ -1,2 +1,15 @@
 public class Fila {
+
+    private No primeiro;
+    private No ultimo;
+
+    public Fila() {
+        primeiro = new No();
+        ultimo = primeiro;
+    }
+
+    public boolean filaVazia(){
+        return (primeiro == null && ultimo == null);
+    }
+
 }
--- Códigos/AulaoTAD-Fila/src/No.java
+++ Códigos/AulaoTAD-Fila/src/No.java
@@ -1,2 +1,14 @@
 public class No {
+
+    public No proximo;
+    public int elemento;
+
+    public No() {
+        this(0);
+    }
+
+    public No(int entradaUsuario) {
+        elemento = entradaUsuario;
+        proximo = null;
+    }
 }
