d1145af204693befe2a37b952d0cb0a99c4deb5f
Altino Júnior <altinoalvesjunior@gmail.com>
2020-11-03 11:30:31 -0300

    ALT: finalizando as classes

--- Códigos/AulaoTAD-Fila/src/Fila.java
+++ Códigos/AulaoTAD-Fila/src/Fila.java
@@ -9,7 +9,32 @@ public class Fila {
     }
 
     public boolean filaVazia(){
-        return (primeiro == null && ultimo == null);
+        return ((primeiro == null) && (ultimo == null));
     }
 
+    public void enfileirar(int elementoUsuario) {
+        ultimo.proximo = new No(elementoUsuario);
+        ultimo = ultimo.proximo;
+    }
+
+    public int desenfileirar() throws Exception {
+        if (filaVazia()) {
+            throw new Exception("Não é possível remover, a fila está vazia!");
+        }
+
+        No temporario = primeiro;
+        primeiro = primeiro.proximo;
+        int resp = primeiro.elemento;
+        temporario.proximo = null;
+        temporario = null;
+
+        return resp;
+    }
+
+    public void imprimir() {
+        for(No i = primeiro.proximo; i != null; i = i.proximo) {
+            System.out.print("[" + i.elemento + "] ");
+        }
+        System.out.println();
+    }
 }
